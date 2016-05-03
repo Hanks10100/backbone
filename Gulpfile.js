25057cdbd8655e5bfe3f761454c9b488ed0abca4
Hanks <zhang.han113@zte.com.cn>
2016-05-03 16:48:26 +0800

    添加 form-view 入口文件

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,7 +6,7 @@ var webpackConfig   = require('./webpack.config.js');
 
 function createTask(srcName, distName) {
     gulp.task(srcName + ':compile', function() {
-        gulp.src('src/' + srcName + '.js')
+        gulp.src('src/' + srcName + '/index.js')
             .pipe(webpack(webpackConfig))
             .pipe(rename(distName + '.js'))
             .pipe(gulp.dest('dist'));
@@ -19,8 +19,10 @@ function createTask(srcName, distName) {
     });
 }
 
+createTask('form-view', 'backbone.formView');
 
 gulp.task('watch', function() {
+    gulp.watch('src/**/*.js', ['form-view:compile']);
 });
 
 gulp.task('default', function() {
