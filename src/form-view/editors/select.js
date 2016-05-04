ce079ff19db8c565a8831c30aeeef244120c8082
Hanks <zhang.han113@zte.com.cn>
2016-05-04 10:55:45 +0800

    实现初始化 select 选项的功能

--- src/form-view/editors/select.js
+++ src/form-view/editors/select.js
@@ -2,6 +2,19 @@ const BaseEditor = require('./base');
 
 const SelectEditor = BaseEditor.extend({
     tagName: 'select',
+    initialize(options = {}, configs = {}) {
+        this.options = _.result(options, 'options');
+        _.each(this.options, (opt, index) => {
+            this.$el.append(
+                $('<option></option>')
+                    .prop('selected', !!opt.selected)
+                    .attr('value', opt.value)
+                    .attr('title', opt.text)
+                    .html(opt.text)
+            );
+        });
+        return this;
+    },
 });
 
 module.exports = {
