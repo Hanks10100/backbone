50a173cf4f8cf76b79c0528867e72bd55a2a2e83
Hanks <zhang.han113@zte.com.cn>
2016-05-04 10:21:13 +0800

    添加初始化表单内容的功能

--- src/form-view/field.js
+++ src/form-view/field.js
@@ -6,6 +6,27 @@ const FieldView = Backbone.View.extend({
     attributes: {
         role: 'form-field',
     },
+
+    // TODO: 添加更详细的表单项的 DOM 结构
+    initialize: function(options, configs) {
+        var name = options.name;
+        this.type = options.type;
+        this.$label = $('<label class="control-label"></label>')
+            .attr('for', name)
+            .attr('role', 'field-label')
+            .html(options.label)
+            // .addClass(getColumnClass({col: '4,3'}))
+
+        this.editor = createEditor.apply(this, arguments);
+        this.$editor = this.editor.$el;
+
+        this.$output = $('<output class="field-output"></output>')
+            .attr('role', 'field-output').hide();
+        var $wrapper = $('<div></div>');//.addClass(getColumnClass({col: '8,9'}));
+
+        this.$el.attr('data-field', name);
+        this.$el.append(this.$label, $wrapper.append(this.editor.$el, this.$output));
+    },
 });
 
 module.exports = FieldView;
--- src/form-view/form.js
+++ src/form-view/form.js
@@ -3,6 +3,50 @@ const FieldView = require('./field');
 
 const FormView = Backbone.View.extend({
     tagName: 'form',
+    className: 'form-horizontal container-fluid',
+    attributes: {
+        enctype: 'multipart/form-data',
+    },
+    events: {
+        'focus [role="form-editor"]': 'handleFocus',
+    },
+    schema: [],
+
+    // 重写父类接口，初始化表单内容
+    setElement(element) {
+        if (/form/i.test(element.tagName)) {
+            this.undelegateEvents();
+            this._setElement(element);
+            this._initFieldElement();
+            this.delegateEvents();
+        } else {
+            // console.warn('element must be a FormElement.');
+        }
+        return this;
+    },
+
+    // 添加表单的内容
+    _initFieldElement() {
+        var self = this;
+        this.$el.append(_.map(this.schema, function(fieldset, index) {
+            return $('<fieldset class="row"></fieldset>')
+                .attr('data-row', index + 1)
+                .append(_.map(fieldset, function(schema, key) {
+                    var $wrapper = $('<div></div>');//.addClass(getColumnClass(schema));
+                    const field = self.createField(key, schema);
+                    return $wrapper.append(field.el);
+                }));
+        }));
+    },
+
+    createField: function(key, schema) {
+        this.fields = this.fields || {};
+
+        var field = new FieldView(_.extend({ name: key }, schema));
+        this.fields[key] = field;
+
+        return field;
+    },
 });
 
 module.exports = FormView;
