0ef9c918ede96554f0c8cca8c03cb04674f10846
Hanks <zhang.han113@zte.com.cn>
2016-05-04 14:10:52 +0800

    使 form view 不破坏 Backbone.View 原有的特性

--- src/form-view/form.js
+++ src/form-view/form.js
@@ -3,46 +3,37 @@ const utils = require('./utils');
 const FieldView = require('./field');
 
 const FormView = Backbone.View.extend({
-    tagName: 'form',
-    className: 'form-horizontal container-fluid',
-    attributes: {
-        enctype: 'multipart/form-data',
-    },
-    events: {
-        'submit': 'submit',
-        'focus [role="form-editor"]': 'handleFocus',
-    },
     schema: [],
 
-    // 重写父类接口，初始化表单内容
-    setElement(element) {
-        if (/form/i.test(element.tagName)) {
-            this.undelegateEvents();
-            this._setElement(element);
-            this._initFieldElement();
-            this.delegateEvents();
-        } else {
-            // console.warn('element must be a FormElement.');
-        }
-        return this;
+    _setElement: function(el) {
+        this.$el = el instanceof Backbone.$ ? el : Backbone.$(el);
+        this.el = this.$el[0];
+        this.$form = this._createFormElement();
+        this.$el.html(this.$form);
     },
 
     // 添加表单的内容
-    _initFieldElement() {
-        this.$el.append(_.map(this.schema, (fieldset, index) =>
-            $('<fieldset class="row"></fieldset>')
-                .attr('data-row', index + 1)
-                .append(_.map(fieldset, (schema, key) =>
-                    $('<div></div>')
-                        .addClass(utils.getColumnClass(schema))
-                        .append(this.createField(key, schema).el)
-                ))
-        ));
+    _createFormElement() {
+        return $('<form class="form-horizontal container-fluid"></form>')
+            .attr({
+                enctype: 'multipart/form-data',
+            })
+            .append(_.map(this.schema, (fieldset, index) =>
+                $('<fieldset class="row"></fieldset>')
+                    .attr('data-row', index + 1)
+                    .append(_.map(fieldset, (options, key) =>
+                        $('<div></div>')
+                            .addClass(utils.getColumnClass(options))
+                            .append(this.createField(key, options).el)
+                    ))
+            ));
     },
 
-    createField(key, schema) {
-        const options = _.extend({ name: key }, schema);
-        const field = new FieldView(options);
+    createField(key, options = {}, configs = {}) {
+        const field = new FieldView(
+            _.extend({ name: key }, options),
+            configs
+        );
 
         this.fields = this.fields || {};
         this.fields[key] = field;
