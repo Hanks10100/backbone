5aa97a3516f7c9445ec28ebd6c3616c4c3c5b7d4
Hanks <zhang.han113@zte.com.cn>
2016-05-04 15:37:52 +0800

    实现生成按钮的功能

--- src/form-view/buttons.js
+++ src/form-view/buttons.js
@@ -1,6 +1,25 @@
 
 const ButtonGroupView = Backbone.View.extend({
     className: 'btn-wrapper',
+
+    initialize(options, configs) {
+        const { buttons, col, offset } = options;
+        this.$btns = [];
+        _.each(buttons, options => {
+            const $btn = this.createButton(options);
+            this.$btns = $btn.add(this.$btns);
+            this.$el.append($btn);
+        });
+    },
+
+    // TODO: 生成多种样式的按钮
+    createButton(options) {
+        const className = 'btn-primary';
+        return $('<button class="btn"></button>')
+            .attr('type', options.type)
+            .addClass(className)
+            .html(options.text)
+    },
 });
 
 module.exports = ButtonGroupView;
