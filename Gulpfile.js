3d02220b1b5b56d6fa38359e462e8c69dcc20bb4
Hanks <zhang.han113@zte.com.cn>
2016-05-04 09:47:26 +0800

    调整脚本的编译选项

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,4 +1,5 @@
 var gulp            = require('gulp');
+var filter          = require('gulp-filter');
 var rename          = require('gulp-rename');
 var uglify          = require('gulp-uglify');
 var webpack         = require('webpack-stream');
@@ -6,20 +7,22 @@ var webpackConfig   = require('./webpack.config.js');
 
 function createTask(srcName, distName) {
     gulp.task(srcName + ':compile', function() {
+        webpackConfig.output = { filename: distName + '.js' };
         gulp.src('src/' + srcName + '/index.js')
             .pipe(webpack(webpackConfig))
-            .pipe(rename(distName + '.js'))
             .pipe(gulp.dest('dist'));
     });
-    gulp.task(srcName + ':release', [srcName + ':compile'], function() {
-        gulp.src('dist/' + srcName + '.js')
+    gulp.task(srcName + ':release', function() {
+        gulp.src('src/' + srcName + '/index.js')
+            .pipe(webpack(webpackConfig))
+            .pipe(filter('*.js'))
             .pipe(uglify())
             .pipe(rename({ suffix: '.min' }))
             .pipe(gulp.dest('dist'));
     });
 }
 
-createTask('form-view', 'backbone.formView');
+createTask('form-view', 'FormView');
 
 gulp.task('watch', function() {
     gulp.watch('src/**/*.js', ['form-view:compile']);
