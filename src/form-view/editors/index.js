726b6004893c1278878216d26c4a69f215bb99c3
Hanks <zhang.han113@zte.com.cn>
2016-05-04 16:45:47 +0800

    使 createEditor 兼容创建 XTag 和 fish 的组件

--- src/form-view/editors/index.js
+++ src/form-view/editors/index.js
@@ -2,15 +2,33 @@ const { CheckboxEditor, RadioEditor } = require('./checks');
 const { InputEditor, TextareaEditor } = require('./input');
 const { SelectEditor } = require('./select');
 
+function parseType(options = {}) {
+    const type = options.type || options.name;
+    const res = type.split('.');
+    return {
+        catagory: res.length > 1 ? res[0] : null,
+        type: _.last(res).toLowerCase(),
+    }
+}
 
 // 创建编辑器视图的工厂函数
 function createEditor(options = {}, configs = {}) {
+    const { catagory, type } = parseType(options);
     var Editor = InputEditor;
-    switch (options.type) {
-        case 'checkbox':     Editor = CheckboxEditor;    break;
-        case 'radio':        Editor = RadioEditor;       break;
-        case 'select':       Editor = SelectEditor;      break;
-        case 'textarea':     Editor = TextareaEditor;    break;
+    switch (catagory) {
+        case 'XTag':
+            if (window.XTag) Editor = XTag.getComponent('x', catagory);
+        break;
+
+        case 'fish':
+        break;
+
+        default: switch (type) {
+            case 'checkbox':     Editor = CheckboxEditor;    break;
+            case 'radio':        Editor = RadioEditor;       break;
+            case 'select':       Editor = SelectEditor;      break;
+            case 'textarea':     Editor = TextareaEditor;    break;
+        }
     }
     return new Editor(options, configs);
 }
